59d3ce552365cc94268e559f6b795f48504e1d93
PatrickM <saluk64007@gmail.com>
2025-07-03 11:42:57 -0700

    Add private journal entries. Design flaw A04 exposes private entries in html to unauthorized users

--- src/pages/models.py
+++ src/pages/models.py
@@ -12,7 +12,7 @@ class JournalEntry(models.Model):
 	text = models.TextField()
 	date = models.DateTimeField(default=datetime.datetime.now)
 
-class PrivateView(models.Model):
-	journal = models.OneToOneField(JournalEntry, on_delete=models.CASCADE)
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+#class PrivateView(models.Model):
+#	journal = models.OneToOneField(JournalEntry, on_delete=models.CASCADE)
+#	user = models.OneToOneField(User, on_delete=models.CASCADE)
 
--- src/pages/views.py
+++ src/pages/views.py
@@ -4,14 +4,25 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.db import transaction
-from .models import JournalEntry, PrivateView
+from .models import JournalEntry
 
 def homePageView(request):
-	entries = reversed(JournalEntry.objects.order_by('date'))
+	entries = reversed(
+		JournalEntry.objects.order_by('date')
+	)
+
+	### OWASP A04 - Uncomment to ensure no unauthorized private journal entries are sent to the front end ###
+	# if request.user.username:
+	# 	entries = [e for e in entries if e.public or e.author == request.user]
+	# else:
+	# 	entries = [e for e in entries if e.public]
+
 	return render(request, 'pages/index.html', {'entries': entries})
 
 def writeView(request):
 	entry = JournalEntry(author=request.user, text=request.POST['entry_text'])
+	if request.POST['public'] == "false":
+		entry.public = False
 	entry.save()
 	return redirect("home")
 
@@ -23,7 +34,7 @@ def deleteView(request):
 
     ### OWASP A01 - Uncomment to ensure user is the author of the journal entry being deleted ###
 	# if not entry.author == request.user:
-	# 	 return redirect("home")
+	# 	return redirect("home")
 
 	entry.delete()
 	return HttpResponse('Item deleted! <a href="/">Back to home</a>')
